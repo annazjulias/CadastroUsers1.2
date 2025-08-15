265bb100704378a9ae6acb887066e71534094e44
Annazjulias <anazjuliasilva15@gmail.com>
2025-08-14 21:44:24 -0300

    frontTerminando

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,5 @@
-// services/api.js
 import axios from "axios";
-
 const api = axios.create({
-  baseURL: "http://localhost:3000", // Porta do seu backend
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3001"
 });
-
 export default api;
